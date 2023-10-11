a3c98769038766b88728772fca669b8ada54cb55
HarishaGB <harshagb555@gmail.com>
2023-10-12 00:46:19 +0530

    3rd

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ setInterval(()=>{
     let currentTime = new Date();
 
     hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    mins.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
 },1000);
